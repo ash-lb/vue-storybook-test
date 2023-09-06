56fb84e785c4017f55ddf2e35732b5c7bc88020a
Adrian <adrian@lifeblue.com>
2023-09-06 12:32:20 -0500

    Styling test component with tw

--- src/modules/MediaText.vue
+++ src/modules/MediaText.vue
@@ -4,16 +4,44 @@
   <div class="tw-container tw-mx-auto">
     <div class="tw-grid tw-grid-cols-12 tw-gap-4">
       <div class="tw-col-span-7">
-        <h4 class="">Header Right</h4>
-        <h2 class="">Faster Connections, Better Conversions</h2>
-        <p class="">
+        <h4 class="tw-text-2xl tw-font-black">Header Right</h4>
+        <h2 class="tw-text-5xl tw-font-black tw-pt-2.5">Faster Connections, Better Conversions</h2>
+        <p class="tw-mt-10">
           Let us help you grow your audience — and get them to convert — with innovative campaigns
           and creative, data-driven strategies. We put the value of our experience to work for you
           to deliver results that exceed your expectations.
         </p>
-        <div class="">
-          <a class="" href="#"> Have a project? Let's talk </a>
-          <a class="" href="#"> Learn More </a>
+        <div class="tw-my-10 tw-flex tw-flex-wrap">
+          <a
+            class="tw-font-black tw-bg-sky-700 tw-text-white tw-py-4 tw-px-10 tw-items-center tw-flex tw-gap-2 hover:tw-bg-sky-900"
+            href="#"
+          >
+            Have a project? Let's talk
+            <svg class="tw-w-4 tw-h-4 tw-fill-white tw-pt-px">
+              <g id="arrow-right-Layer_2" data-name="Layer 2">
+                <g id="arrow-right-Layer_1-2" data-name="Layer 1">
+                  <polygon
+                    points="0 1.5 1.5 0 8.06 6.56 1.5 13.12 0 11.62 5.06 6.56 0 1.5"
+                  ></polygon>
+                </g>
+              </g>
+            </svg>
+          </a>
+          <a
+            class="tw-font-black tw-text-sky-700 tw-py-4 tw-px-6 tw-underline tw-decoration-2 tw-items-center tw-flex tw-gap-2 tw-group hover:tw-text-sky-900"
+            href="#"
+          >
+            Learn More
+            <svg class="tw-w-4 tw-h-4 tw-fill-sky-700 tw-pt-px group-hover:tw-fill-sky-900">
+              <g id="arrow-right-Layer_2" data-name="Layer 2">
+                <g id="arrow-right-Layer_1-2" data-name="Layer 1">
+                  <polygon
+                    points="0 1.5 1.5 0 8.06 6.56 1.5 13.12 0 11.62 5.06 6.56 0 1.5"
+                  ></polygon>
+                </g>
+              </g>
+            </svg>
+          </a>
         </div>
       </div>
       <div class="tw-col-span-5">
