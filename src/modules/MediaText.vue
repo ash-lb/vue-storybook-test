e1f61875622bcc385553f0b4326a08a68756a35d
Adrian <adrian@lifeblue.com>
2023-09-11 17:28:13 -0500

    Added props in component and args in story

--- src/modules/MediaText.vue
+++ src/modules/MediaText.vue
@@ -1,22 +1,32 @@
-<script setup></script>
+<script setup>
+defineProps({
+  subheading: String,
+  heading: String,
+  copy: String,
+  call_to_action_primary: Object,
+  call_to_action_secondary: Object,
+  media_type: String,
+  image: Object,
+  video: Object,
+  orientation: String
+})
+</script>
 
 <template>
   <div class="tw-container tw-mx-auto">
     <div class="tw-grid tw-grid-cols-12 tw-gap-4">
       <div class="tw-col-span-7">
-        <h4 class="tw-text-2xl tw-font-black">Header Right</h4>
-        <h2 class="tw-text-5xl tw-font-black tw-pt-2.5">Faster Connections, Better Conversions</h2>
+        <h4 class="tw-text-2xl tw-font-black">{{ subheading }}</h4>
+        <h2 class="tw-text-5xl tw-font-black tw-pt-2.5">{{ heading }}</h2>
         <p class="tw-mt-10">
-          Let us help you grow your audience — and get them to convert — with innovative campaigns
-          and creative, data-driven strategies. We put the value of our experience to work for you
-          to deliver results that exceed your expectations.
+          {{ copy }}
         </p>
         <div class="tw-my-10 tw-flex tw-flex-wrap">
           <a
             class="tw-font-black tw-bg-sky-700 tw-text-white tw-py-4 tw-px-10 tw-items-center tw-flex tw-gap-2 hover:tw-bg-sky-900"
-            href="#"
+            :href="call_to_action_primary.url"
           >
-            Have a project? Let's talk
+            {{ call_to_action_primary.text }}
             <svg class="tw-w-4 tw-h-4 tw-fill-white tw-pt-px">
               <g id="arrow-right-Layer_2" data-name="Layer 2">
                 <g id="arrow-right-Layer_1-2" data-name="Layer 1">
@@ -29,9 +39,9 @@
           </a>
           <a
             class="tw-font-black tw-text-sky-700 tw-py-4 tw-px-6 tw-underline tw-decoration-2 tw-items-center tw-flex tw-gap-2 tw-group hover:tw-text-sky-900"
-            href="#"
+            :href="call_to_action_secondary.url"
           >
-            Learn More
+            {{ call_to_action_secondary.text }}
             <svg class="tw-w-4 tw-h-4 tw-fill-sky-700 tw-pt-px group-hover:tw-fill-sky-900">
               <g id="arrow-right-Layer_2" data-name="Layer 2">
                 <g id="arrow-right-Layer_1-2" data-name="Layer 1">
@@ -45,12 +55,7 @@
         </div>
       </div>
       <div class="tw-col-span-5">
-        <img
-          class=""
-          alt="Descriptive alt text that describes the image."
-          src="https://via.placeholder.com/900x900?text=FPO?fit=crop&amp;crop=faces,entropy,center&amp;q=70&amp;auto=format&amp;or=0&amp;fm=jpeg&amp;w=355&amp;h=266"
-          loading="lazy"
-        />
+        <img class="" :alt="image.url" :src="image.url" loading="lazy" />
       </div>
     </div>
   </div>
--- src/stories/MediaText.stories.js
+++ src/stories/MediaText.stories.js
@@ -6,23 +6,36 @@ export default {
   component: MediaText,
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-    onClick: {},
-    size: {
-      control: {
-        type: 'select',
-      },
-      options: ['small', 'medium', 'large'],
-    },
+    // backgroundColor: {
+    //   control: 'color',
+    // },
+    // onClick: {},
+    // size: {
+    //   control: {
+    //     type: 'select',
+    //   },
+    //   options: ['small', 'medium', 'large'],
+    // },
   },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/vue/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
-    label: 'Media Text',
+    subheading: 'Header Right',
+    heading: 'Faster Connections, Better Conversions',
+    copy: 'Let us help you grow your audience — and get them to convert — with innovative campaigns and creative, data-driven strategies. We put the value of our experience to work for you to deliver results that exceed your expectations.',
+    call_to_action_primary: {
+      text: 'Have a project? Let\'s talk',
+      url: '#',
+    },
+    call_to_action_secondary: {
+      text: 'Learn More',
+      url: '#',
+    },
+    image: {
+      url: 'https://via.placeholder.com/900x900?text=FPO?fit=crop&crop=faces,entropy,center&q=70&auto=format&or=0&fm=jpeg&w=355&h=266',
+      alt: 'Descriptive alt text that describes the image.',
+    },
   },
 };
